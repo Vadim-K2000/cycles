cc88a92b39ee26c6583d287e223502d80e5c613c
Vadim Kovalenko <vadimkovalenko.zp.ua@gamil.com>
2020-06-25 17:00:22 +0300

    Сommit homework
    Do the first four tasks

--- index.js
+++ index.js
@@ -1,93 +1,38 @@
-const car1 = {
-    color: 'Blue',
-    isRepainted: false,
-    brand: 'BMW',
-    model: 'prototype10',
-    doorsCount: 5,
-    seatsNumber: 7,
-    maxSpeed: 250,
-    engien: {
-        type: 'ICE',
-        volume: 1.6,
-    },
-    goDrive: function goDrive() {
-        alert( 'wroom - wroom !!!' );
-    },
-};
+'use strict'
 
-const car2 = {
-    color: 'White',
-    isRepainted: false,
-    brand: 'Skoda',
-    model: 'prototype10',
-    doorsCount: 4,
-    seatsNumber: 7,
-    maxSpeed: 230,
-    engien: {
-        type: 'ICE',
-        volume: 1.5,
-    },
-};
+//while
+// 1. Вывести # столько раз, сколько указал пользователь.
 
-const car3 = {
-    color: 'Black',
-    isRepainted: true,
-    brand: 'Opel',
-    model: 'prototype10',
-    doorsCount: 4,
-    seatsNumber: 7,
-    maxSpeed: 220,
-    engien: {
-        type: 'ICE',
-        volume: 1.7,
-    },
-};
-
-
-const cat = {
-    name: 'Barsik',
-    age: 10,
-    run() {
-        alert( 'Run Barsik, Run!!!' );
-    },
-
-    hide() {
-        alert( 'Hide Barsik!' );
-    },
-};
-
-function doBackFlip() {
-    alert( 'Bak Flip' );
+function enterLattice(n) {
+    let i = 0;
+    while ( i++ < n ) {
+        console.log( '#' );
+    }
 }
 
-cat.dobackFlip = doBackFlip;
+//2. Пользователь ввел число, а на экран вывелись все числа от введенного до 0.
+function func1(n) {
+    let i = 0;
+    let result = n;
+    while ( i <= result ) {
+        console.log( result-- );
+    }
+}
+//3. Запросить число и степень. Возвести число в указанную степень и вывести результат.
 
-function User( name, surname ) {
-    this.name = name;
-    this.surname = surname;
+function power(num1, num2) {
+    let i = 0;
+    let res = 1;
+    while (num2 > i++ ) {
+        res *= num1;
+    }
+    return res;
 }
 
-const user1 = new User( 'Ivan', 'Ivanov' );
-const user2 = new User( 'Maria', 'Ivanovna' );
+//4. Запросить 2 числа и найти все общие делители.
 
-function Country( title, population, size ) {
-    this.title = title;
-    this.population = population;
-    this.size = size;
-    this.density = function () {
-        return this.population / this.size;
-    };
-}
 
-const country1 = new Country( 'Ukraine', 41980000, 603628 );
-const country2 = new Country( 'USA', 328200000, 9834000 );
 
-function Ids( voltage, resist, ) {
-    this.voltage = voltage;
-    this.resist = resist;
-    this.power = function () {
-        return this.voltage / this.resist;
-    };
-}
 
-const x = new Ids( 10, 5 );
+
+
